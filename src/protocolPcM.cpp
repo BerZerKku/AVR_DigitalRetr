ea604e60e8dad7b1fcfb3791d4e0ca0a90fd9422
bear <bear@prosoftsystems.ru>
2018-11-16 16:26:01 +0500

    Изменена работа с дискретными входами. Теперь сброс и установка производятся записью в разные адреса. Добавлены тесты для protocolPcM.
    Signed-off-by: bear <bear@prosoftsystems.ru>

--- src/protocolPcM.cpp
+++ src/protocolPcM.cpp
@@ -6,12 +6,47 @@
  */
 #include "protocolPcM.h"
 
+// Сброс состояний дискретных входов и выходов.
+void TProtocolPcM::reset() {
+	for(uint8_t i = 0; i < D_INPUT_MAX; i++) {
+		dIn[i] = 0x0000;
+	}
+
+	for(uint8_t i = 0; i < D_OUTPUT_MAX; i++) {
+		dOut[i] = 0x0000;
+	}
+};
+
+// Чтение состояний дискретных входов.
+uint16_t TProtocolPcM::getDI(dInput_t din) const {
+	uint16_t val = 0;
+	if ((din >= D_INPUT_16_01) && (din < D_INPUT_MAX)) {
+		val = dIn[din];
+	}
+	return val;
+}
+
+// Запись состояний дискретных выходов.
+void TProtocolPcM::setDO(dOutput_t dout, uint16_t val) {
+	if ((dout >= D_OUTPUT_16_01) && (dout < D_OUTPUT_MAX)) {
+		dOut[dout] = val;
+	}
+}
+
 /**	Чтение внутренних регистров.
  *
  *	Проводится проверка корректности адреса. Если он находится в допустимом
  *	диапазоне, возвращается текущее значение регистра.
  *
- *	C неиспользуемых адресов будет считано 0xFFFF.
+ *	Доступные адреса для считывания:
+ *	- ADR_HOLDING_REG_DI_16_01_ON	Состояние дискретных входов с 16 по 1.
+ *	- ADR_HOLDING_REG_DI_32_17_ON	Состояние дискретных входов с 32 по 17.
+ *	- ADR_HOLDING_REG_DI_16_01_OFF	Состояние дискретных входов с 16 по 1.
+ *	- ADR_HOLDING_REG_DI_32_17_OFF	Состояние дискретных входов с 32 по 17.
+ *	Т.е. состояние одних и тех же дискретных входов можно считать с разных
+ *	адресов.
+ *
+ *	C неиспользуемых адресов будет считано 0x0000.
  *
  *	@param adr Адрес регистра.
  *	@param val [out] Значение регистра.
@@ -20,15 +55,24 @@
  */
 TProtocolModbus::CHECK_ERR TProtocolPcM::readRegister(uint16_t adr, uint16_t &val) {
 	//  проверка адресов
-	if ((adr <= ADR_REG_MIN) || (adr >= ADR_REG_MAX))
+	if ((adr <= ADR_HOLDING_REG_MIN) || (adr >= ADR_HOLDING_REG_MAX))
 		return CHECK_ERR_ADR;
 
-	// по умолчанию будет возвращено 0xFFFF
-	val = 0xFFFF;
-	if (adr == ADR_COM_16_01) {
-		val = comTx[ADR_COM_16_01];
-	}  else if (adr == ADR_COM_32_17) {
-		val = comTx[ADR_COM_32_17];
+	val = 0x0000;
+	switch((adrHoldingReg_t) adr) {
+		case ADR_HOLDING_REG_DI_16_01_ON:	// DOWN
+		case ADR_HOLDING_REG_DI_16_01_OFF:
+			val = dIn[D_INPUT_16_01];
+			break;
+
+		case ADR_HOLDING_REG_DI_32_17_ON:	// DOWN
+		case ADR_HOLDING_REG_DI_32_17_OFF:
+			val = dIn[D_INPUT_32_17];
+			break;
+
+		case ADR_HOLDING_REG_MIN:	// DOWN
+		case ADR_HOLDING_REG_MAX:
+			break;
 	}
 
 	return CHECK_ERR_NO;
@@ -39,7 +83,11 @@ TProtocolModbus::CHECK_ERR TProtocolPcM::readRegister(uint16_t adr, uint16_t &va
  *	Проводится проверка корректности адреса. Если он находится в допустимом
  *	диапазоне, возвращается текущее значение регистра.
  *
- *	C неиспользуемых адресов будет считано 0xFFFF.
+ *	Доступные адреса для считывания:
+ *	- ADR_INPUT_REG_DI_16_01 Состояние дискретных выходов с 16 по 1.
+ *	- ADR_INPUT_REG_DI_32_17 Состояние дискретных выходов с 32 по 17.
+ *
+ *	C неиспользуемых адресов будет считано 0x0000.
  *
  *	@param adr Адрес регистра.
  *	@param val [out] Значение регистра.
@@ -48,17 +96,23 @@ TProtocolModbus::CHECK_ERR TProtocolPcM::readRegister(uint16_t adr, uint16_t &va
  */
 TProtocolModbus::CHECK_ERR TProtocolPcM::readIRegister(uint16_t adr, uint16_t &val) {
 	//  проверка адресов
-	if ((adr <= ADR_REG_MIN) || (adr >= ADR_REG_MAX))
+	if ((adr <= ADR_INPUT_REG_MIN) || (adr >= ADR_INPUT_REG_MAX))
 		return CHECK_ERR_ADR;
 
-	// по умолчанию будет возвращено 0xFFFF
-	val = 0xFFFF;
-	if (adr == ADR_COM_16_01) {
-//		val = comRx[ADR_COM_16_01]; FIXME
-		val = comTx[ADR_COM_16_01];
-	}  else if (adr == ADR_COM_32_17) {
-//		val = comRx[ADR_COM_32_17]; FIXME
-		val = comTx[ADR_COM_32_17];
+	// по умолчанию будет возвращено 0x0000
+	val = 0x0000;
+	switch((adrInputReg_t) adr) {
+		case ADR_INPUT_REG_DO_16_01:
+			val = dOut[D_OUTPUT_16_01];
+			break;
+
+		case ADR_INPUT_REG_DO_32_17:
+			val = dOut[D_OUTPUT_32_17];
+			break;
+
+		case ADR_INPUT_REG_MIN:	// DOWN
+		case ADR_INPUT_REG_MAX:
+			break;
 	}
 
 	return CHECK_ERR_NO;
@@ -81,13 +135,29 @@ TProtocolModbus::CHECK_ERR TProtocolPcM::readIRegister(uint16_t adr, uint16_t &v
  */
 TProtocolModbus::CHECK_ERR TProtocolPcM::writeRegister(uint16_t adr, uint16_t val) {
 
-	if ((adr <= ADR_REG_MIN	) || (adr >= ADR_REG_MAX))
+	if ((adr <= ADR_HOLDING_REG_MIN) || (adr >= ADR_HOLDING_REG_MAX))
 		return CHECK_ERR_ADR;
 
-	if (adr == ADR_COM_16_01) {
-		comTx[ADR_COM_16_01] = val;
-	} else if (adr == ADR_COM_32_17) {
-		comTx[ADR_COM_32_17] = val;
+	switch((adrHoldingReg_t) adr) {
+		case ADR_HOLDING_REG_DI_16_01_ON:
+			dIn[D_INPUT_16_01] |= val;
+			break;
+
+		case ADR_HOLDING_REG_DI_32_17_ON:
+			dIn[D_INPUT_32_17] |= val;
+			break;
+
+		case ADR_HOLDING_REG_DI_16_01_OFF:
+			dIn[D_INPUT_16_01] &= ~val;
+			break;
+
+		case ADR_HOLDING_REG_DI_32_17_OFF:
+			dIn[D_INPUT_32_17] &= ~val;
+			break;
+
+		case ADR_HOLDING_REG_MIN:	// DOWN
+		case ADR_HOLDING_REG_MAX:
+			break;
 	}
 
 	return CHECK_ERR_NO;
